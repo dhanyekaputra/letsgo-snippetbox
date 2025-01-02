054ace28edbbc414d9b2ccb80c70d4fc07a94b2f
dhnyekaputra <pramesta.dhany@gmail.com>
2025-01-02 14:05:33 +0700

    flag-addres

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	//define command line with name addr, default 4000
+	// and some explaining what the flag control
+	// flag will be stored in the addr cariable at runtime
+	//VALUE IN POINTER
+	addr := flag.String("addr", ":4000", "HTTP Network Address")
+
+	//importantly we use flag.Parse func to parse command-line flag
+	//this read the command line flag and assign to addr
+	//this should be called before using addr var
+	//otherwise the value will always be ":4000"
+	//error will be terminated
+	flag.Parse()
 
 	mux := http.NewServeMux()
 
-	// create files server which serves files out of the directory
-	// relative to the directory root
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
@@ -19,8 +30,9 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetview)
 	mux.HandleFunc("/snippet/create", snippetcreate)
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	//value is a pointer, so we need to dereference the pointer
+	log.Println("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 
 }
