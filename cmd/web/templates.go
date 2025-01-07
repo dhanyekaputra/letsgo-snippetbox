4ceba28af6e85baa5d83978053559d4dcfd72ad1
dhnyekaputra <pramesta.dhany@gmail.com>
2025-01-07 11:56:14 +0700

    5.3 Caching Template part 2 Automatic Parsing Partials

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -19,6 +19,9 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	// initialize a new map to act as the cache
 	cache := map[string]*template.Template{}
 
+	// use the filepath.Glob() to get a slice of all filepath
+	// that match the patter "./ui/html/pages/*.tmpl"
+	// [ui/html/pages/home.tmpl ui/html/pages/view.tmpl]
 	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
 
 	if err != nil {
@@ -30,13 +33,26 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		//assign it to the name variable
 		name := filepath.Base(page)
 
-		file := []string{
-			"./ui/html/base.tmpl.html",
-			"./ui/html/partials/nav.tmpl.html",
-			page,
+		// file := []string{
+		// 	"./ui/html/base.tmpl.html",
+		// 	"./ui/html/partials/nav.tmpl.html",
+		// 	page,
+		// }
+
+		// parse the base template file into a template set
+		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		if err != nil {
+			return nil, err
 		}
 
-		ts, err := template.ParseFS(file...)
+		// call parsegblob *on this template set* to add any partials
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		// call parsefiles *on this template sed* to add the page template
+		ts, err = ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
